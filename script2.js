87aa745bd5df1b1e87345e2ee0eb3081b2de70cb
Chris Maffei <cmaffei720@gmail.com>
2020-05-16 18:45:07 -0400

    final commit with leaderboard updated

---
+++ script2.js
@@ -0,0 +1,9 @@
+var startOver = document.querySelector("#startOver")
+var clear = document.querySelector("#clear")
+var leaderboard = document.querySelector("#leaderboard")
+
+var scores = localStorage.getItem(score)
+var name = localStorage.getItm(userName)
+
+document.body.appendChild(scores)
+document.body.appendChild(name)

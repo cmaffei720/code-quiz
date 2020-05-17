ac52beba0fd95e2b241652eee3cc7b3459bce98f
Chris Maffei <cmaffei720@gmail.com>
2020-05-17 15:55:39 -0400

    final final commit

--- script2.js
+++ script2.js
@@ -2,8 +2,18 @@ var startOver = document.querySelector("#startOver")
 var clear = document.querySelector("#clear")
 var leaderboard = document.querySelector("#leaderboard")
 
-var scores = localStorage.getItem(score)
-var name = localStorage.getItm(userName)
+var scores = localStorage.getItem("score")
+var name = localStorage.getItem("userName")
+
 
 document.body.appendChild(scores)
 document.body.appendChild(name)
+
+startOver.setAttribute("href", "./index.html")
+
+clear.addEventListener("click", function(event){
+    localStorate.setItem("userName", "")
+    localStorage.setItem("score", "")
+})
+
+console.log(userName)
